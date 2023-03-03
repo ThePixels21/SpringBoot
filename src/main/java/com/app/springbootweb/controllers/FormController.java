89aa7d34ec59a1ac309383a61fc269694a0c3ace
ThePixels21 <santigamer521@gmail.com>
2023-03-03 10:29:13 -0500

    Corregido

--- src/main/java/com/app/springbootweb/controllers/FormController.java
+++ src/main/java/com/app/springbootweb/controllers/FormController.java
@@ -2,6 +2,7 @@ package com.app.springbootweb.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -24,8 +25,10 @@ public class FormController {
     }
 
     @PostMapping("/form")
-    public String procesar(@Valid Usuario usuario, Model model){
-        model.addAttribute("titulo", "Resultado formulario");
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model){
+        if(result.hasErrors()){
+            model.addAttribute("titulo", "Resultado formulario");
+        }
         return "resultado";
     }
 }
